9b2ac7e881e0b28944a1bce04ff24f9ccb8c99b5
yingkailiang <yingkailiang0920@gmail.com>
2013-08-14 12:36:59 +0800

    test iframe

--- app/CrossAppTest/js/app.js
+++ app/CrossAppTest/js/app.js
@@ -26,3 +26,41 @@ cursor.onerror = function() {
 }//end func display
 
 
+// Check battery
+function readBattery()
+{
+  var battery = navigator.battery;
+  //alert(battery.level);
+  if(battery)
+  {
+    batteryState = document.getElementById("batteryState");
+    // Set listeners for changes
+    batteryState.innerHTML = 'Battery: '+battery.level*100+'%';
+    battery.addEventListener('levelchange', function() { 
+    batteryState.innerHTML = 'Battery: '+battery.level*100+'%';
+    if(battery.level<0.15)
+    {
+      alert("low battery power");
+    }
+    },false);
+  }
+  else
+    {
+     alert("battery status cannot read");
+    }
+}//end function
+ 
+
+     // Geolocation
+     var geolocation = document.getElementById("geolocation"),
+        geolocationDisplay = document.getElementById("geolocation-display");
+        geolocation.onclick = function () {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                geolocationDisplay.innerHTML = position.coords.latitude + "  " + position.coords.longitude;
+            },
+            function (position) {
+                geolocationDisplay.innerHTML = "Failed to get your current location";
+            });
+        };
+ 
+ 
